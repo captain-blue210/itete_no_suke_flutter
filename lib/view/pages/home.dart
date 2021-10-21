ad2b5e6c525b6b366dbaee67f201f3f01ce8be09
captain-blue210 <captain.blue210@gmail.com>
2021-10-21 23:09:17 +0900

    style : add record list

--- lib/view/pages/home.dart
+++ lib/view/pages/home.dart
@@ -13,6 +13,42 @@ class Home extends StatelessWidget {
       body: Center(
         child: ListView(
           children: <Widget>[
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.tag_faces),
+                title: Text('2021/10/22'),
+                subtitle: Text('1. dummy1\n2. dummy2\n3. dummy3'),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                isThreeLine: true,
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.tag_faces),
+                title: Text('2021/10/23'),
+                subtitle: Text('1. dummy1\n2. dummy2\n3. dummy3'),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                isThreeLine: true,
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.tag_faces),
+                title: Text('2021/10/23'),
+                subtitle: Text('1. dummy1\n2. dummy2\n3. dummy3'),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                isThreeLine: true,
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.tag_faces),
+                title: Text('2021/10/23'),
+                subtitle: Text('1. dummy1\n2. dummy2\n3. dummy3'),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                isThreeLine: true,
+              ),
+            ),
             TextButton(
                 onPressed: () => Navigator.push(
                     context,
