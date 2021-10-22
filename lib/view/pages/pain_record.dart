f7a74ea9f07dd5f6a59d127d57eea873c02bc12d
captain-blue210 <captain.blue210@gmail.com>
2021-10-22 22:24:37 +0900

    style : update pain_record_page to SingleChildScrollView

--- lib/view/pages/pain_record.dart
+++ lib/view/pages/pain_record.dart
@@ -10,84 +10,129 @@ class PainRecordPage extends StatefulWidget {
 class _PainRecordPageState extends State<PainRecordPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('体調記録'),
-      ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            const Text(
-              'いまどんなかんじ？',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+    return Padding(
+      padding:
+          EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              const Text(
+                'いまどんなかんじ？',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.tag_faces),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.tag_faces),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.tag_faces),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.tag_faces),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-            const Text(
-              'おくすりのんだ？',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.tag_faces),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.tag_faces),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.tag_faces),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.tag_faces),
+                    onPressed: () {},
+                  ),
+                ],
               ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
+              const Text(
+                'おくすりのんだ？',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'お薬1',
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'お薬2',
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'お薬3',
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'お薬4',
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'お薬5',
+                    ),
+                  ),
+                ],
+              ),
+              const Text(
+                '痛いところは？',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
                 TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'お薬1',
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    minLines: 1,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'メモ',
+                    )),
+              ]),
+              const Text(
+                'メモ',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+                SizedBox(
+                  height: 100,
+                  child: TextField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'メモ',
+                    ),
                   ),
                 ),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'お薬2',
-                  ),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'お薬3',
-                  ),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'お薬4',
-                  ),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'お薬5',
-                  ),
-                ),
-              ],
-            ),
-          ],
+              ]),
+              IconButton(
+                icon: Icon(Icons.add_a_photo),
+                color: Colors.grey,
+                iconSize: 50,
+                onPressed: () {},
+              ),
+            ],
+          ),
         ),
       ),
     );
