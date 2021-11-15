42ec0b07ab98a5dc9de6082937cdbe1eaa78c0d3
Aoki Hayato <67002842+captain-blue210@users.noreply.github.com>
2021-11-15 21:55:03 +0900

    feat : implements dropdowns (#56)
    close #54

--- lib/presentation/request/painRecord/PainRecordRequestParam.dart
+++ lib/presentation/request/painRecord/PainRecordRequestParam.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/cupertino.dart';
+import 'package:itete_no_suke/model/bodyParts/body_part.dart';
 import 'package:itete_no_suke/model/medicine/medicine.dart';
 import 'package:itete_no_suke/model/painRecord/pain_level.dart';
 
 class PainRecordRequestParam with ChangeNotifier {
   PainLevel _painLevel = PainLevel.noPain;
   List<Medicine>? _medicines = <Medicine>[];
+  List<BodyPart>? _bodyParts = <BodyPart>[];
 
   PainRecordRequestParam();
 
   List<Medicine>? getMedicines() => _medicines;
+  List<BodyPart>? getBodyParts() => _bodyParts;
 
   set medicines(Medicine medicine) {
     if (!_medicines!.contains(medicine)) {
       _medicines!.add(medicine);
+    }
+  }
+
+  set bodyParts(BodyPart bodypart) {
+    if (!_bodyParts!.contains(bodypart)) {
+      _bodyParts!.add(bodypart);
     }
   }
 
