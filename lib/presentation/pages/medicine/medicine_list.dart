5d46eeca7595db0520953817875670a04c172a0c
Aoki Hayato <67002842+captain-blue210@users.noreply.github.com>
2022-04-30 21:29:14 +0900

    #87 delete item on cascade (#105)
    close #87 
    * fix : #87 delete a medicine on cascade
    
    * design : change home icon to note icon
    
    * fix : #87 delete a bodyparts on cascade

--- lib/presentation/pages/medicine/medicine_list.dart
+++ lib/presentation/pages/medicine/medicine_list.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:itete_no_suke/presentation/widgets/medicine/medicine_card_list.dart';
+import 'package:itete_no_suke/application/medicine/medicine_service.dart';
+import 'package:itete_no_suke/model/medicine/medicine.dart';
+import 'package:itete_no_suke/presentation/widgets/medicine/medicine_card.dart';
+import 'package:provider/src/provider.dart';
 
 class MedicineList extends StatefulWidget {
   const MedicineList({Key? key}) : super(key: key);
@@ -11,6 +15,40 @@ class MedicineList extends StatefulWidget {
 class _MedicineListState extends State<MedicineList> {
   @override
   Widget build(BuildContext context) {
-    return const MedicineCardList();
+    return StreamBuilder<QuerySnapshot<Medicine>>(
+      // TODO need to use real userID
+      stream: context.read<MedicineService>().getMedicinesByUserID(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return ListView.builder(
+            itemCount: snapshot.data!.size,
+            itemBuilder: (context, index) {
+              return Dismissible(
+                key: UniqueKey(),
+                direction: DismissDirection.endToStart,
+                background: Container(
+                  alignment: Alignment.centerRight,
+                  color: Colors.red,
+                  child: const Padding(
+                      padding: EdgeInsets.fromLTRB(10.0, 0.0, 20.0, 0.0),
+                      child: Icon(Icons.delete, color: Colors.white)),
+                ),
+                onDismissed: (direction) {
+                  context
+                      .read<MedicineService>()
+                      .deleteMedicine(snapshot.data!.docs[index].id);
+                },
+                child: MedicineCard(
+                  name: snapshot.data!.docs[index].data().name!,
+                  medicineID: snapshot.data!.docs[index].id,
+                ),
+              );
+            },
+          );
+        } else {
+          return ListView();
+        }
+      },
+    );
   }
 }
