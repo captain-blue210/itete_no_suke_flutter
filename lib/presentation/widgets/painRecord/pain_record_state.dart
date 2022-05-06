a2d231dc6720c6ce00dd39a526a2671fdfa437fb
captain-blue210 <captain.blue210@gmail.com>
2022-05-06 23:45:41 +0900

    chore : remove unused logs

--- lib/presentation/widgets/painRecord/pain_record_state.dart
+++ lib/presentation/widgets/painRecord/pain_record_state.dart
@@ -4,9 +4,7 @@ class PainRecordState extends ChangeNotifier {
   bool _isLoading = false;
 
   void toggleLoading() {
-    print('before isLoading: ${_isLoading}');
     _isLoading = !_isLoading;
-    print('before isLoading: ${_isLoading}');
     notifyListeners();
   }
 
